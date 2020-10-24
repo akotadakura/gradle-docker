fb9adbcea8f1c905af4c9eaaab2376472da4c19b
akotadakura <saitono@outlook.com>
2020-10-24 18:32:21 +0900

    applied Json loggin with logstash-slf4j-logback

--- src/main/java/info/akotadakura/App.java
+++ src/main/java/info/akotadakura/App.java
@@ -1,11 +1,17 @@
 package info.akotadakura;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class App {
+    private final static Logger logger = LoggerFactory.getLogger(App.class);
+
     public String getGreeting() {
         return "Hello world.";
     }
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
+        logger.info("json logging: {}", "succeed!!");
     }
 }
