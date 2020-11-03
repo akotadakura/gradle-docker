79eac4c8fc2a7252b91b7281836b61ac16948775
akotadakura <saitono@outlook.com>
2020-11-03 18:18:31 +0900

    switched test framework: JUnit 4 -> 5

--- src/test/java/info/akotadakura/utils/SampleUtilTest.java
+++ src/test/java/info/akotadakura/utils/SampleUtilTest.java
@@ -1,22 +1,27 @@
 package info.akotadakura.utils;
 
 import static info.akotadakura.utils.SampleUtil.sampleText;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.hamcrest.MatcherAssert.assertThat;
+//import static org.hamcrest.Matchers.is;
+//
+//import org.junit.Test;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class SampleUtilTest {
 
   @Test
-  public void returnThinkingMachineName() {
+  void returnThinkingMachineName() {
     String songName = sampleText(true);
-    assertThat(songName, is("1- Measurement Of Thoughts"));
+//    assertThat(songName, is("1- Measurement Of Thoughts"));
+    assertEquals("1- Measurement Of Thoughts", songName);
   }
-
+//
   @Test
   public void returnZeus() {
     String songName = sampleText(false);
-    assertThat(songName, is("2- Deus Ex Machina"));
+//    assertThat(songName, is("2- Deus Ex Machina"));
+    assertEquals("2- Deus Ex Machina", songName);
   }
 }
